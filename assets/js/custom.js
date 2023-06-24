0d568c8f729e4a296f098cae6f0433df9eb88157
Viram Goyani <vaghasiyab347@gmail.com>
2023-06-24 06:41:39 -0700

    product details
    done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,6 +15,36 @@ $(".count-number").each(function () {
       }
     );
 });
+// multi slider js start
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+// multi slider js end
 // quantity css start
 var QtyInput = (function () {
 	var $qtyInputs = $(".qty-input");
