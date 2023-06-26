4af0186d369b04947cc1343f4ff8ad023a9690ae
Viram Goyani <vaghasiyab347@gmail.com>
2023-06-26 08:28:33 -0700

    working on

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -182,3 +182,9 @@ var swiper2 = new Swiper(".mySwiper2", {
   },
 });
 // multi slider js end
+// NICE SELECT JS START
+$(document).ready(function () {
+  $("select:not(.ignore)").niceSelect();
+  FastClick.attach(document.body);
+});
+ // NICE SELECT JS END
